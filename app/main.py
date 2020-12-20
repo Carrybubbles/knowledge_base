dd279b65e76985b55462194a996c16f1fa63b9d3
Федин Артем Алексеевич <jfedin95@gmail.com>
2020-12-20 19:46:49 +0700

    добавил рест для получения всех моделей в платформе h2o

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from h2o import h2o
 
 app = Flask(__name__)
+h2o.init(nthreads=-1,ip="172.18.79.222", port=52762)
 
 @app.route('/ok')
-def index():
+def health():
     return "Hello, World!"
+
+@app.route('/model')
+def model():
+    return h2o.models()
